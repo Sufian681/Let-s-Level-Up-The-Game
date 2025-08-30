88ad9adce53405949b1533085746ccc5fec8acbc
Sufi <sufiansonic@gmail.com>
2025-08-30 18:41:13 +0900

    g

--- Spritecollision.py
+++ Spritecollision.py
@@ -1,25 +1,39 @@
 import pygame
-import random
+import random   
 
+# Constants
 SCREEN_WIDTH, SCREEN_HEIGHT = 500, 400
 MOVEMENT_SPEED = 5
 FONT_SIZE = 72
 
 pygame.init()
 
+# Load background image
 background_image = pygame.transform.scale(
-    pygame.image.load("bg.png"), (SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.image.load("bg.png"), (SCREEN_WIDTH, SCREEN_HEIGHT)
+)
 
-font = pygame.sysfont.SysFont("Times New Roman", FONT_SIZE)
+# Font setup
+font = pygame.font.SysFont("Times New Roman", FONT_SIZE)
 
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, color, height, width):
         super().__init__()
+        self.colors = [pygame.Color("black"), pygame.Color("red"), pygame.Color("green"), pygame.Color("yellow"), pygame.Color("purple")]
+        self.color_index = self.colors.index(color) if color in self.colors else 0
+        self.height = height
+        self.width = width
         self.image = pygame.Surface([width, height])
-        self.image.fill(
-            pygame.Color("dodgerblue"))
-        pygame.draw.rect(self.image, color, pygame.Rect(0, 0, width, height))
+        self.update_color()
         self.rect = self.image.get_rect()
+        
+    def update_color(self):
+        self.image.fill(pygame.Color("dodgerblue"))
+        pygame.draw.rect(self.image, self.colors[self.color_index], pygame.Rect(0, 0, self.width, self.height))
+        
+    def change_color(self):
+        self.color_index = (self.color_index + 1) % len(self.colors)
+        self.update_color()
         
     def move(self, x_change, y_change):
         self.rect.x = max(
@@ -28,6 +42,7 @@ class Sprite(pygame.sprite.Sprite):
             min(self.rect.y + y_change, SCREEN_HEIGHT - self.rect.height), 0
         ) 
 
+# Set up display and sprites
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Sprite Collision")
 all_sprites = pygame.sprite.Group()
@@ -36,6 +51,7 @@ sprite1 = Sprite(pygame.Color("black"), 20, 30)
 sprite1.rect.x, sprite1.rect.y = random.randint(
     0, SCREEN_WIDTH - sprite1.rect.width), random.randint(0, SCREEN_HEIGHT - sprite1.rect.height)
 all_sprites.add(sprite1)
+
 sprite2 = Sprite(pygame.Color("red"), 20, 30)
 sprite2.rect.x, sprite2.rect.y = random.randint(
     0, SCREEN_WIDTH - sprite2.rect.width), random.randint(0, SCREEN_HEIGHT - sprite2.rect.height)
@@ -43,19 +59,25 @@ all_sprites.add(sprite2)
 
 running, won = True, False
 clock = pygame.time.Clock()
+last_color_change = pygame.time.get_ticks()
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_x):
             running = False
             
+    current_time = pygame.time.get_ticks()
+    if current_time - last_color_change >= 1000:
+        sprite1.change_color()
+        sprite2.change_color()
+        last_color_change = current_time
+
     if not won:
         keys = pygame.key.get_pressed()    
         x_change = (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * MOVEMENT_SPEED
         y_change = (keys[pygame.K_DOWN] - keys[pygame.K_UP]) * MOVEMENT_SPEED
         
         sprite1.move(x_change, y_change)
-        
         
         if sprite1.rect.colliderect(sprite2.rect):
             all_sprites.remove(sprite2)
@@ -73,9 +95,3 @@ while running:
     clock.tick(90)
     
 pygame.quit()
-            
-            
-        
-
-    
-   
